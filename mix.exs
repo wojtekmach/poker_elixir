f1e660f550bc4847b2658986ec7cdee6ad800057
Wojtek Mach <wojtekmach@users.noreply.github.com>
2024-11-03 23:43:13 +0100

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,39 +1,27 @@
 defmodule Poker.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/wojtekmach/poker_elixir"
+
   def project do
     [
       app: :poker,
-      version: "0.0.2",
+      version: @version,
       elixir: "~> 1.1",
-      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      docs: fn ->
-        {ref, 0} = System.cmd("git", ["rev-parse", "--verify", "--quiet", "HEAD"])
-        [source_ref: ref, readme: "README.md", main: "Poker"]
+      docs: [
+        source_url: @source_url,
+        source_ref: "v#{@version,
+        readme: "README.md",
+        main: "Poker"]
       end,
       description: "An Elixir library to work with Poker hands.",
       package: package()
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
-  def application do
-    [applications: [:logger]]
-  end
-
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:ex_doc, "~> 0.34", only: :dev}
@@ -44,7 +32,7 @@ defmodule Poker.Mixfile do
     [
       licenses: ["MIT"],
       maintainers: ["Wojtek Mach"],
-      links: %{"GitHub" => "https://github.com/wojtekmach/poker_elixir"}
+      links: %{"GitHub" => @source_url}
     ]
   end
 end
